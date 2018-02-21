2ae3ddfd11690b3a66ef2c13655cb4a12e85b2d6
“romcioromcio <sobczynskir@wp.pl>
2018-02-21 16:43:19 +0100

    19.1 Spring WEB

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 $(document).ready(function() {
-  const apiRoot = 'http://boiling-savannah-48508.herokuapp.com/v1/task/';
-  const trelloApiRoot = 'http://boiling-savannah-48508.herokuapp.com/v1/trello/';
+  const apiRoot = 'https://boiling-savannah-48508.herokuapp.com/v1/task/';
+  const trelloApiRoot = 'https://boiling-savannah-48508.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
 
